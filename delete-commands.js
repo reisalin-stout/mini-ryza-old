a5bc48e2b34fba36b65423fcac9af4f26e4f61db
reisalin-stout <adventurous.ryza@gmail.com>
2024-03-01 21:59:48 +0100

    New token :(

--- delete-commands.js
+++ delete-commands.js
@@ -1,8 +1,12 @@
 const url = "https://discord.com/api/v10/applications/1213208038698262678/commands/1213223549385318492";
 
+let mytoken1 = "MTIxMzIwODAzODY5ODI2MjY3OA.GJnu9I";
+let mytoken2 = ".KPCvRJBUttrb4mFtivY4JA3Fo4ssv3fvtlqhGk";
+
+let mytoken = "Bot " + mytoken1 + mytoken2;
 // For authorization, you can use either your bot token
 const headers = {
-  Authorization: "Bot MTIxMzIwODAzODY5ODI2MjY3OA.GNaqah.lNS3iiWurqIbMCFhaWRZJl-S45ch4eJOetxlFY",
+  Authorization: mytoken,
 };
 
 fetch(url, {
