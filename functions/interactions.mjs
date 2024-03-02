70f44c115f0e09bb50b413435274ae47432268cf
reisalin-stout <adventurous.ryza@gmail.com>
2024-03-02 15:54:16 +0100

    maybe this is better idk

--- functions/interactions.mjs
+++ functions/interactions.mjs
@@ -1,18 +1,55 @@
-const { validateRequest } = require("discord-interactions");
+import express from "express";
+import serverless from "serverless-http";
+import { verifyKey, verifyKeyMiddleware } from "discord-interactions";
 
-exports.handler = async (event) => {
-  // Validate the incoming request
-  const isValidRequest = validateRequest(event);
-
-  if (!isValidRequest) {
+function interact(command) {
+  if (command.type == 1) {
+    return { type: 1 };
+  } else {
+    let response_content;
+    switch (command.name) {
+      case "hello":
+        response_content = "Hello there!";
+        break;
+      case "bye":
+        response_content = "Goodbye!";
+        break;
+      case "echo":
+        let original_message = command.options[0].value;
+        response_content = `Echoing: ${original_message}`;
+        break;
+      default:
+        response_content = "No Message";
+    }
     return {
-      statusCode: 401,
-      body: JSON.stringify({ error: "Invalid request" }),
+      type: 4,
+      data: { content: response_content },
     };
   }
+}
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ type: 1 }),
-  };
-};
+const app = express();
+
+app.get("/interactions", (req, res) => {
+  res.send("Hello Site");
+});
+app.post("/interactions", (req, res) => {
+  const signature = req.get("X-Signature-Ed25519");
+  const timestamp = req.get("X-Signature-Timestamp");
+  const isValidRequest = verifyKey(req.rawBody, signature, timestamp, process.env.PUBLIC_KEY);
+  if (!isValidRequest) {
+    return res.status(401).end("Bad request signature");
+  }
+
+  const message = req.body;
+
+  try {
+    console.log("Request:", message);
+    res.json(interact(message));
+  } catch (error) {
+    console.error("Error parsing request:", error);
+    res.status(400).json({ error: "Bad request" });
+  }
+});
+
+export const handler = serverless(app);
