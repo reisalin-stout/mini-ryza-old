c70cf6e27051eb5e3afdcd764db8b14b89254ff5
reisalin-stout <adventurous.ryza@gmail.com>
2024-03-04 10:12:26 +0100

    trying some new stuff to locally host

--- functions/interact.mjs
+++ functions/interact.mjs
@@ -7,57 +7,6 @@ import fetch from "node-fetch";
 const app = express();
 const router = Router();
 
-function ehp(values) {
-  let hp = values[0].value;
-  let def = values[1].value;
-  let result = hp / (1 - def / (def + 1200));
-  return `HP: ${hp} - Defense: ${def} => eHP ${result}`;
-}
-
-function reverseEhp(values) {
-  let target = values[0].value * values[1].value;
-  let base_hp = values[2].value;
-  let base_def = values[3].value;
-  let trinket_hp = 15000 + 6000;
-  let trinket_def = 1500 + 600;
-
-  let percent = 200;
-  let max = 0;
-  let spread = [0, 0];
-  for (var a = 0; a < percent; a++) {
-    let perc_hp = a;
-    let perc_def = percent - perc_hp;
-    var HP = trinket_hp + (base_hp * (100 + perc_hp)) / 100;
-    var DEF = trinket_def + (base_def * (100 + perc_def)) / 100;
-
-    var eHP = Math.round(HP / (1 - DEF / (DEF + 1200)));
-
-    if (eHP > max) {
-      max = eHP;
-      spread[0] = perc_hp;
-      spread[1] = perc_def;
-    }
-  }
-
-  let ratio = spread[0] - spread[1];
-  for (let a = 0; ; a++) {
-    let perc_hp = a + ratio;
-    let perc_def = a;
-    var HP = trinket_hp + (base_hp * (100 + perc_hp)) / 100;
-    var DEF = trinket_def + (base_def * (100 + perc_def)) / 100;
-    var eHP = Math.round(HP / (1 - DEF / (DEF + 1200)));
-
-    if (eHP >= target) {
-      console.log(`eHP => ${eHP}`);
-      console.log(`Ratio at ${HP} - ${DEF}`);
-      console.log(`Ratio at ${100 + perc_hp} - ${100 + perc_def}`);
-      return `eHP: ${eHP} => 
-      - HP ${HP} (+${perc_hp}% +${trinket_hp}f)
-      Defense: ${DEF} (+${perc_def}% +${trinket_def}f)`;
-    }
-  }
-}
-
 async function callExternal(function_name, options) {
   let url = `https://mini-ryza.netlify.app/.netlify/functions/${function_name}`;
 
@@ -84,38 +33,22 @@ async function callExternal(function_name, options) {
 async function interact(command) {
   let response;
   switch (command.name) {
-    case "ehp":
-      response = ehp(command.options);
-      break;
-    case "reverse-ehp":
-      response = reverseEhp(command.options);
-      break;
-    case "titan-rank":
-      let url = `http://v-g-msl-rank.p-msl.com:10831/rank/top/?board_id=${command.options[1].value}&meta_key=rank_${command.options[1].value}`;
-      console.log(url);
-      response = "No clan found";
-      await fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.results);
-          let clans = data.results;
-          if (clans.length > 0) {
-            clans.forEach((element) => {
-              console.log(element[`rank_${command.options[1].value}`]);
-              if (element[`rank_${command.options[1].value}`] == command.options[0].value) {
-                response = element;
-              }
-            });
-          }
-        });
-      console.log(response);
-      break;
     case "bye":
+      let url = "https://api.jsonbin.io/v3" + "/b/" + process.env.BIN_ID + "/latest";
+      const headers = {
+        "Content-Type": "application/json",
+        "X-Master-Key": process.env.BIN_KEY,
+      };
+
+      await fetch(url, {
+        method: "GET",
+        headers: headers,
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error:", error));
+
       response = "Goodbye!";
-      break;
-    case "echo":
-      let original_message = command.options[0].value;
-      response = `Echoing: ${original_message}`;
       break;
     default:
       response = "Command not found";
@@ -136,7 +69,6 @@ router.post("/interactions", verifyKeyMiddleware(process.env.PUBLIC_KEY), async 
     }
     */
     let result = await interact(command);
-    console.log(result);
     res.send({
       type: 4,
       data: { content: result },
